221d5d468a2b3009fe9e54269fee7cdfeddee372
Tomas <tomashilbert@hotmail.com>
2021-01-23 13:28:16 -0500

    Modificacion de server.js

--- server.js
+++ server.js
@@ -8,23 +8,13 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
-const livereload = require('livereload');
-const connectLiveReload = require('connect-livereload');
-
 const publicDirectory = path.join(__dirname, '/public');
-const viewsDirectory = path.join(__dirname, '/views');
-
-const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(viewsDirectory);
 
 //Express HBS Engine
 const partialsDirectory = path.join(__dirname, '/views/partials');
 hbs.registerPartials(partialsDirectory);
 app.set('view engine', 'hbs');
 
-
-
-app.use(connectLiveReload());
 app.use(express.static(publicDirectory));
 
 app.get('/', (req, res) => {
